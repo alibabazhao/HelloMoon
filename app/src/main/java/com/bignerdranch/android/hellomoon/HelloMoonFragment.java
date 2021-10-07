3f6df15b029636db2ce5c18c9236d341e9f376e4
Ali Zhao <0604aliz@gmail.com>
2021-10-07 10:52:39 -0400

    Add listerner to play and stop

--- app/src/main/java/com/bignerdranch/android/hellomoon/HelloMoonFragment.java
+++ app/src/main/java/com/bignerdranch/android/hellomoon/HelloMoonFragment.java
@@ -12,13 +12,31 @@ public class HelloMoonFragment extends Fragment {
     private Button mPlayButton;
     private Button mStopButton;
 
+    private AudioPlayer mPlayer=new AudioPlayer();
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup parent, Bundle saveInstanceState){
         View v=inflater.inflate(R.layout.fragment_hello_moon, parent, false);
 
         mPlayButton=(Button)v.findViewById(R.id.hellomoon_playButton);
-        mStopButton=(Button)v.findViewById(R.id.hellomoon_stopButton);
+        mPlayButton.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                mPlayer.play(getActivity());
+            }
+        });
 
+        mStopButton=(Button)v.findViewById(R.id.hellomoon_stopButton);
+        mStopButton.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                mPlayer.stop();
+            }
+        });
         return v;
+    }
+
+    @Override
+    public void onDestroy(){
+        super.onDestroy();
+        mPlayer.stop();
     }
 }
